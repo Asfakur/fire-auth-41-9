c7e1e27f5ccdfddb73d00dceff876d4f27c47b97
Asfakur <saikat.hap@gmail.com>
2021-03-15 22:39:17 +0600

    41-9 Module Summary

---
+++ src/firebase.cofig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCH1wn-XDfJjR3fvm6UcdpnnPQsZJJHhFo",
+    authDomain: "ema-john-simple-aa2c4.firebaseapp.com",
+    projectId: "ema-john-simple-aa2c4",
+    storageBucket: "ema-john-simple-aa2c4.appspot.com",
+    messagingSenderId: "706238219154",
+    appId: "1:706238219154:web:f2bcd0578e8158278bf74b"
+  };
+  
+  export default firebaseConfig;
